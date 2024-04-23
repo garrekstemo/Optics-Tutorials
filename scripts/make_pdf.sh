a65c7bf0be0eb8b019d4f4aa29275414f7f7e922
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2024-04-23 11:53:04 +0900

    Update PDF script to output in font that supports unicode characters

--- scripts/make_pdf.sh
+++ scripts/make_pdf.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
-pandoc ../src/*.md -f commonmark --pdf-engine=lualatex -V colorlinks=true --toc -o ../pdf/optics-tutorials.pdf
+pandoc ../src/*.md -f commonmark --pdf-engine=lualatex \
+    -V colorlinks=true \
+    -V mainfont="Helvetica" \
+    --toc \
+    -o ../pdf/optics-tutorials.pdf
 cd ../pdf
 echo "PDF created at $PWD/optics-tutorials.pdf"
